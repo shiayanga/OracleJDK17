1c434f18b467ddbff632027eaa97917973650a63
shiayanga <1499271883@qq.com>
2022-08-29 11:57:50 +0800

    add source

---
+++ jdk.compiler/com/sun/tools/javac/resources/version.java
@@ -0,0 +1,11 @@
+package com.sun.tools.javac.resources;
+
+public final class version extends java.util.ListResourceBundle {
+    protected final Object[][] getContents() {
+        return new Object[][] {
+            { "full", "17.0.4.1+1-LTS-2" },
+            { "jdk", "17.0.4.1" },
+            { "release", "17.0.4.1" },
+        };
+    }
+}
